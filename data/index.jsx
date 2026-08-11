63b890b6277f1a951f20adc1f63ead0d19465ffa
paarijaat366 <paarijaat366@gmail.com>
2026-08-11 20:13:43 +0530

    Changes related to responsiveness

--- data/index.jsx
+++ data/index.jsx
@@ -26,7 +26,7 @@ export const index =
       id: 4,
       title: 'Tech enthusiast with a passion for development.',
       description: 'The Inside Scoop',
-      className: 'sm:col-span-1 md:col-span-2 lg:col-span-1 lg:row-span-2 bg-background border-gray-600 flex items-end justify-end bg-[url(/b4.svg)] bg-auto bg-center p-4',
+      className: 'sm:col-span-1 md:col-span-2 lg:col-span-1 lg:row-span-1 bg-background border-gray-600 flex items-end justify-end bg-[url(/b4.svg)] bg-auto bg-center p-4',
     },
     {
       id: 5,
@@ -39,7 +39,7 @@ export const index =
       id: 6,
       title: 'Do you want to start a project together?',
       description: 'The piece of art is just a contact away',
-      className: 'sm:col-span-1 md:col-span-2 lg:col-span-1 bg-background border-gray-600 flex items-end justify-end h-full bg-gradient-to-r from-blue-500 via-pink-500 to-purple-500 animate-motion-gradient overflow-hidden p-4',
+      className: 'sm:col-span-1 md:col-span-1 lg:col-span-1 bg-background border-gray-600 flex items-end justify-end h-full bg-gradient-to-r from-blue-500 via-pink-500 to-purple-500 animate-motion-gradient overflow-hidden p-4',
     },
 ]
 
